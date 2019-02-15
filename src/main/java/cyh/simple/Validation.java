61a1aabc40e9cb076b6395c02d17e4c0bf37c058
iintothewind <iintothewind@yeah.net>
2019-02-15 20:54:44 +0800

    changed indent

--- src/main/java/cyh/simple/Validation.java
+++ src/main/java/cyh/simple/Validation.java
@@ -10,41 +10,41 @@ import java.util.stream.StreamSupport;
 
 @FunctionalInterface
 public interface Validation<T, E> {
-    static <T, E> Validation<T, E> valid() {
-        return (T t) -> Collections.emptyList();
-    }
+  static <T, E> Validation<T, E> valid() {
+    return (T t) -> Collections.emptyList();
+  }
 
-    Iterable<E> validate(final T t);
+  Iterable<E> validate(final T t);
 
-    default Validation<T, E> and(final Validation<? super T, ? extends E> other) {
-        return (T t) -> Optional
-            .ofNullable(other)
-            .filter(o -> Optional.ofNullable(o.validate(t)).map(Iterable::iterator).filter(Iterator::hasNext).isPresent())
-            .map(that -> Stream
-                .concat(
-                    StreamSupport.stream(Optional
-                        .ofNullable(validate(t))
-                        .map(Iterable::spliterator)
-                        .orElse(Collections.<E>emptyList().spliterator()), true),
-                    StreamSupport.stream(that.validate(t).spliterator(), true))
-                .collect(Collectors.toList()))
-            .orElse(StreamSupport
-                .stream(Optional
-                    .ofNullable(validate(t)).map(Iterable::spliterator)
-                    .orElse(Collections.<E>emptyList().spliterator()), true)
-                .collect(Collectors.toList()));
-    }
+  default Validation<T, E> and(final Validation<? super T, ? extends E> other) {
+    return (T t) -> Optional
+      .ofNullable(other)
+      .filter(o -> Optional.ofNullable(o.validate(t)).map(Iterable::iterator).filter(Iterator::hasNext).isPresent())
+      .map(that -> Stream
+        .concat(
+          StreamSupport.stream(Optional
+            .ofNullable(validate(t))
+            .map(Iterable::spliterator)
+            .orElse(Collections.<E>emptyList().spliterator()), true),
+          StreamSupport.stream(that.validate(t).spliterator(), true))
+        .collect(Collectors.toList()))
+      .orElse(StreamSupport
+        .stream(Optional
+          .ofNullable(validate(t)).map(Iterable::spliterator)
+          .orElse(Collections.<E>emptyList().spliterator()), true)
+        .collect(Collectors.toList()));
+  }
 
-    default Validation<T, E> or(final Validation<? super T, ? extends E> other) {
-        return (T t) -> {
-            if (Optional.ofNullable(validate(t)).filter(iterable -> iterable.iterator().hasNext()).isPresent()
-                && Optional.ofNullable(other).map(o -> o.validate(t)).filter(iterable -> iterable.iterator().hasNext()).isPresent()) {
-                return Stream
-                    .concat(StreamSupport.stream(validate(t).spliterator(), true), StreamSupport.stream(other.validate(t).spliterator(), true))
-                    .collect(Collectors.toList());
-            } else {
-                return Collections.emptyList();
-            }
-        };
-    }
+  default Validation<T, E> or(final Validation<? super T, ? extends E> other) {
+    return (T t) -> {
+      if (Optional.ofNullable(validate(t)).filter(iterable -> iterable.iterator().hasNext()).isPresent()
+        && Optional.ofNullable(other).map(o -> o.validate(t)).filter(iterable -> iterable.iterator().hasNext()).isPresent()) {
+        return Stream
+          .concat(StreamSupport.stream(validate(t).spliterator(), true), StreamSupport.stream(other.validate(t).spliterator(), true))
+          .collect(Collectors.toList());
+      } else {
+        return Collections.emptyList();
+      }
+    };
+  }
 }
--- src/main/java/cyh/simple/ValidationUtils.java
+++ src/main/java/cyh/simple/ValidationUtils.java
@@ -11,47 +11,47 @@ import java.util.stream.StreamSupport;
 
 
 public interface ValidationUtils {
-    static <T, E> Validation<T, E> check(final Predicate<T> predicate, final E error) {
-        Objects.requireNonNull(predicate, "predicate is required");
-        Objects.requireNonNull(error, "error message is required");
-        return (T input) -> {
-            if (predicate.test(input)) {
-                return Collections.emptyList();
-            } else {
-                return Collections.singletonList(error);
-            }
-        };
-    }
+  static <T, E> Validation<T, E> check(final Predicate<T> predicate, final E error) {
+    Objects.requireNonNull(predicate, "predicate is required");
+    Objects.requireNonNull(error, "error message is required");
+    return (T input) -> {
+      if (predicate.test(input)) {
+        return Collections.emptyList();
+      } else {
+        return Collections.singletonList(error);
+      }
+    };
+  }
 
-    static <T, E> Validation<T, E> check(final Predicate<T> predicate, final Function<T, E> function) {
-        Objects.requireNonNull(predicate, "predicate is required");
-        Objects.requireNonNull(function, "error message is required");
-        return (T input) -> {
-            if (predicate.test(input)) {
-                return Collections.emptyList();
-            } else {
-                return Collections.singletonList(function.apply(input));
-            }
-        };
-    }
+  static <T, E> Validation<T, E> check(final Predicate<T> predicate, final Function<T, E> function) {
+    Objects.requireNonNull(predicate, "predicate is required");
+    Objects.requireNonNull(function, "error message is required");
+    return (T input) -> {
+      if (predicate.test(input)) {
+        return Collections.emptyList();
+      } else {
+        return Collections.singletonList(function.apply(input));
+      }
+    };
+  }
 
-    static <T> Validation<Iterable<T>, String> checkAll(final Validation<T, String> v) {
-        return (Iterable<T> iterable) -> StreamSupport.stream(iterable.spliterator(), true)
-            .flatMap(t -> {
-                try {
-                    return StreamSupport.stream(v.validate(t).spliterator(), true);
-                } catch (Throwable throwable) {
-                    return Stream.of(String.format("got error %s when validate %s", throwable.getMessage(), t));
-                }
-            }).distinct().collect(Collectors.toList());
-    }
+  static <T> Validation<Iterable<T>, String> checkAll(final Validation<T, String> v) {
+    return (Iterable<T> iterable) -> StreamSupport.stream(iterable.spliterator(), true)
+      .flatMap(t -> {
+        try {
+          return StreamSupport.stream(v.validate(t).spliterator(), true);
+        } catch (Throwable throwable) {
+          return Stream.of(String.format("got error %s when validate %s", throwable.getMessage(), t));
+        }
+      }).distinct().collect(Collectors.toList());
+  }
 
-    static <T> Validation<T, String> checkEqual(final T expected) {
-        return check(actual -> Objects.equals(expected, actual), target -> String.format("%s should equal to %s", target, expected));
-    }
+  static <T> Validation<T, String> checkEqual(final T expected) {
+    return check(actual -> Objects.equals(expected, actual), target -> String.format("%s should equal to %s", target, expected));
+  }
 
-    static Validation<String, String> checkStringNotEmpty(final String error) {
-        return check(input -> Optional.ofNullable(input).filter(s -> !s.isEmpty()).isPresent(), error);
-    }
+  static Validation<String, String> checkStringNotEmpty(final String error) {
+    return check(input -> Optional.ofNullable(input).filter(s -> !s.isEmpty()).isPresent(), error);
+  }
 
 }
