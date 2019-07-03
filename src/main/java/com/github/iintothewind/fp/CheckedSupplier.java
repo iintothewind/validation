8303326cc152cede7607b7615b412dfb88c9f380
iintothewind <iintothewind@yeah.net>
2019-07-03 21:51:18 +0800

    removed fp package

--- src/main/java/com/github/iintothewind/fp/CheckedSupplier.java
+++
@@ -1,6 +0,0 @@
-package com.github.iintothewind.fp;
-
-@FunctionalInterface
-public interface CheckedSupplier<T> {
-  T get() throws Throwable;
-}
--- src/main/java/com/github/iintothewind/fp/ChkRunnable.java
+++
@@ -1,6 +0,0 @@
-package com.github.iintothewind.fp;
-
-@FunctionalInterface
-public interface ChkRunnable {
-  void run() throws Throwable;
-}
